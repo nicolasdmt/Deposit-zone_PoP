84deae3766ccf9cc3e45b2867a41b042b2a2d71d
nicolas_dmt <LbjCavs94>
2020-10-07 18:56:11 +0300

    Test3

---
+++ Lesson_03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int A = 0;
+
+        }
+    }
+}
